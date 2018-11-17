0ebbc2e725042c6df5034b40b0747f3a9218ef50
DmitriyS87 <schukindmitriy@gmail.com>
2018-11-18 01:32:47 +0800

    удалили лишнее, поправили комменты git

--- js/stat.js
+++ js/stat.js
@@ -1,7 +1,7 @@
     window.renderStatistics = function(ctx,names,times) {
         var maxTime=times[0];
         var maxIndex=0;
-        var chartDeviderConst;
+        var chartDeviderConst;//постоянная - пропорция для отрисовки диаграммы
         //Сздаем квадратное облако для вывода результата
         ctx.fillStyle='rgba(0, 0, 0, 0.7)';
         ctx.fillRect(110,20,420,270);
@@ -11,34 +11,29 @@
         var messageStats='Ура Вы победили!\nСписок результатов:'.split('\n');
         ctx.font ='16px Pt Mono';
         ctx.fillStyle='#000';
-        //this.alert(messageStats[1]);
-        for (var i=0; i<2; i++) {
-            ctx.fillText(messageStats[i],120,30+i*20,380);
+        for (var i=0; i<messageStats.length; i++) {
+            ctx.fillText(messageStats[i],120,40+i*20,380);
         }
-         // Прочитаем массив со временем и вычислим максимальную высоту для прорисовки
+         // Прочитаем массив со временем и вычислим максимальное время - самая высокая гистограмма на будущем графике
         for (var i=1;i<names.length;i++) {
             if (times[i]>maxTime) {
                 maxTime=times[i];
                 maxIndex=i;
             };
         };
-        //получаем пропорцию для остальных значений массива
+        //высота гистограммы задана в 150px, значения в игре имеют другую систему исчисления. Получаем пропорцию для отрисовки
         chartDeviderConst=Math.round(maxTime/150);
-        var figureDistance=90;
+        var figureDistance=90; //ширина столбика 40px + расстояние между ними 50px. Константа участвует в вычислении положения следующего столбика
         var chartItemHeight; 
-       // var getColor=function() {
-        //    var CountBlue=Math.random();
-          //  var stringColor = 'rgb(0,0,' + Math.ceil(CountBlue) + ')';
-       //     return stringColor
-       // }    
+
         for (var i=0; i<names.length;i++) {
             chartItemHeight=times[i]/chartDeviderConst;
-            ctx.fillStyle='rgb(0, 0, ' + Math.ceil(Math.random()*255) + ')';//getColor();
-            if (names[i]=='Вы') {ctx.fillStyle='rgba(255, 0, 0, 1)'};
-            ctx.fillRect(160+i*figureDistance,240-chartItemHeight,40,chartItemHeight);
-            ctx.fillStyle='#000';
-            ctx.fillText(names[i],160+i*figureDistance+20-names[i].length/2*8,260);
-            ctx.fillText(Math.round(times[i]).toString(),160+i*figureDistance,240-chartItemHeight-9);
+            ctx.fillStyle='rgb(0, 0, ' + Math.ceil(Math.random()*255) + ')';//задаем рандом на насыщенность синим
+            if (names[i]=='Вы') {ctx.fillStyle='rgba(255, 0, 0, 1)'};//Наше значение устанавливаем красным
+            ctx.fillRect(160+i*figureDistance,240-chartItemHeight,40,chartItemHeight);//рисуем столбик гистограммы
+            ctx.fillStyle='#000';//задаем цвет шрифта надписей
+            ctx.fillText(names[i],160+i*figureDistance+20-names[i].length/2*8,260);//подписываем количество очков/время игрока
+            ctx.fillText(Math.round(times[i]).toString(),160+i*figureDistance,240-chartItemHeight-9);//подписываем имя игрока под его результатом
         }
 
 
