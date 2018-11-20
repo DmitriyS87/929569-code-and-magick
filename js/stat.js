9ee8dead383a8826b02e3308f37f309a1fbbe2c9
DmitriyS87 <schukindmitriy@gmail.com>
2018-11-20 13:24:41 +0800

    исправили оформление констант

--- js/stat.js
+++ js/stat.js
@@ -1,61 +1,81 @@
 'use strict';
 
+// функция возвращает делитель для отрисовки диаграммы
+var getDevider = function (times, heightPx) {
+  var maxTime = times[0];
+  for (var i = 1; i < times.length; i++) {
+    if (times[i] > maxTime) {
+      maxTime = times[i];
+    }
+  }
+  return Math.round(maxTime / heightPx);
+};
+
+// Функция вычисляет произвольную насыщенность цвета в rgb()
+var getColorSaturation = function (minSaturation) {
+  return minSaturation + Math.ceil(Math.random() * (255 - minSaturation));
+};
+
+// Функция округляет входящее значение и возвращает его в виде строки
+var getTimeString = function (time) {
+  return Math.round(time).toString();
+};
+
 window.renderStatistics = function (ctx, names, times) {
   // Вводные данные задания module2-task1:
-  var heightDiagram = 150;
+  var HEIGHTDIAGRAM = 150;
   var message = 'Ура Вы победили!\nСписок результатов:';
-  var barWidth = 40;
-  var spaceBetweenBar = 50;
-  var currentPlayerName = 'Вы';
-  var mainResultsX = 110;
-  var mainResultsY = 20;
+  var BARWIDTH = 40;
+  var SPACEBETWEENBAR = 50;
+  var CURRENTPLAYERNAME = 'Вы';
+  var MAINRESULTSX = 110;
+  var MAINRESULTSY = 20;
+  var MAINRESULTWIDTH = 420;
+  var MAINRESULTHEIGHT = 270;
+  var MINSATURATION = 0;
+  var RESULTSHADOWX = 10;
+  var RESULTSHADOWY = 10;
+  var ROWHEIGHT = 20;
+  var MESSAGEX = 120;
+  var MESSAGEY = 40;
+  var SYMBOLWIDHTPX = 8;
+  var DIAGRAMPADDINGLEFT = 40;
+  var SCORESTEXTMARGIN = 9;
+  var DIAGRAMPADDINGBOTTOM = 30;
+  // Рисуем поле для вывода результатов
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(mainResultsX, mainResultsY, 420, 270);
+  ctx.fillRect(MAINRESULTSX, MAINRESULTSY, MAINRESULTWIDTH, MAINRESULTHEIGHT);
   ctx.fillStyle = 'white';
-  ctx.fillRect(mainResultsX - 10, mainResultsY - 10, 420, 270);
+  ctx.fillRect(MAINRESULTSX - RESULTSHADOWX, MAINRESULTSY - RESULTSHADOWY, MAINRESULTWIDTH, MAINRESULTHEIGHT);
   // Печатаем текст
   var messageArray = message.split('\n');
   ctx.font = '16px Pt Mono';
   ctx.fillStyle = '#000';
+  var messageMaxWidth = MAINRESULTWIDTH - (MESSAGEX - MAINRESULTSX) * 2;
   for (var stringIndex = 0; stringIndex < messageArray.length; stringIndex++) {
-    ctx.fillText(messageArray[stringIndex], 120, 40 + stringIndex * 20, 380);
+    ctx.fillText(messageArray[stringIndex], MESSAGEX, MESSAGEY + stringIndex * ROWHEIGHT, messageMaxWidth);
   }
-  // функция возвращает делитель для отрисовки диаграммы
-  var getDevider = function (heightPx) {
-    var maxTime = times[0];
-    for (var i = 1; i < names.length; i++) {
-      if (times[i] > maxTime) {
-        maxTime = times[i];
-      }
-    }
-    return Math.round(maxTime / heightPx);
-  };
   // высота гистограммы задана в 150px, Результаты игроков имеют другую систему исчисления. Получаем пропорцию для отрисовки гистограммы результатов
-  var chartDevider = getDevider(heightDiagram);
+  var chartDevider = getDevider(times, HEIGHTDIAGRAM);
   // Вычисляем отступ по оси Х между соседними столбиками диаграммы
-  var nextBarX = barWidth + spaceBetweenBar;
+  var nextBarX = BARWIDTH + SPACEBETWEENBAR;
   // Переменная для хранения высоты столбика диаграммы
   var heightCurrentBar;
-  // Функция вычисляет произвольную насыщенность цвета в rgb()
-  var getColorSaturation = function () {
-    return Math.ceil(Math.random() * 255);
-  };
   // Рисуем диаграмму с результатами игроков
+  var barX = MAINRESULTSX + DIAGRAMPADDINGLEFT;
+  var barY = MAINRESULTHEIGHT - DIAGRAMPADDINGBOTTOM;
   for (var i = 0; i < names.length; i++) {
     heightCurrentBar = times[i] / chartDevider;
-    ctx.fillStyle = 'rgb(0, 0, ' + getColorSaturation() + ')';
+    ctx.fillStyle = 'rgb(0, 0, ' + getColorSaturation(MINSATURATION) + ')';
     // Цвет диаграммы результатов текущего игрока устанавливаем красным
-    if (names[i] === currentPlayerName) {
+    if (names[i] === CURRENTPLAYERNAME) {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)'
       ;
     }
-    var getTimeString = function (j) {
-      return Math.round(times[j]).toString();
-    };
-    ctx.fillRect(mainResultsX + 40 + i * nextBarX, 240 - heightCurrentBar, 40, heightCurrentBar);// рисуем столбик гистограммы
+    ctx.fillRect(barX + i * nextBarX, barY - heightCurrentBar, BARWIDTH, heightCurrentBar);// рисуем столбик гистограммы
     ctx.fillStyle = '#000';// задаем цвет шрифта надписей
-    ctx.fillText(names[i], mainResultsX + 40 + i * nextBarX + 20 - names[i].length / 2 * 8, 260);// подписываем имя игрока под его результатом
-    ctx.fillText(getTimeString(i), mainResultsX + 40 + i * nextBarX, 240 - heightCurrentBar - 9);// подписываем количество очков/время игрока
+    ctx.fillText(names[i], barX + i * nextBarX + BARWIDTH / 2 - names[i].length / 2 * SYMBOLWIDHTPX, barY + ROWHEIGHT);// подписываем имя игрока под его результатом
+    ctx.fillText(getTimeString(times[i]), barX + i * nextBarX, barY - heightCurrentBar - SCORESTEXTMARGIN);// подписываем количество очков/время игрока
   }
 
 
