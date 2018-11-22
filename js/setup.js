f406c358c40c10e90c5e9f1dc49e2f284a60c93d
DmitriyS87 <schukindmitriy@gmail.com>
2018-11-22 22:13:15 +0800

    внес изменения в код

--- js/setup.js
+++ js/setup.js
@@ -10,46 +10,42 @@ var PersonsData = {
   coatColors: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
   eyesColors: ['black', 'red', 'blue', 'yellow', 'green']
 };
-// random function: return random item from imput massive data
-var getRandomChoise = function (massiveData, massiveDataCustom) {
+
+var getRandomItem = function (massiveData) {
   var randomIndex = Math.ceil(Math.random() * (massiveData.length - 1));
-  var resultString;
-  if (massiveDataCustom !== undefined) {
-    var randomIndex2 = Math.ceil(Math.random() * (massiveDataCustom.length - 1));
-    var first = Math.round(Math.random());
-    switch (first) {
-      case 1: resultString = massiveDataCustom[randomIndex2] + ' ' + massiveData[randomIndex];
-        break;
-      default: resultString = massiveData[randomIndex] + ' ' + massiveDataCustom[randomIndex2];
-        break;
-    }
+  return massiveData[randomIndex];
+};
+
+var getOneOrZero = function () {
+  return Math.round(Math.random());
+};
+
+var nameSurnameReverse = function (names, surNames) {
+  if (getOneOrZero() < 1) {
+    return getRandomItem(names) + ' ' + getRandomItem(surNames);
   } else {
-    resultString = massiveData[randomIndex];
+    return getRandomItem(surNames) + ' ' + getRandomItem(names);
   }
-  return resultString;
 };
 
 // function return random person specification in object
-var getRandPerson = function (objPersons) {
+var getRandomWizard = function (objPersons) {
   var personData = {
-    name: getRandomChoise(objPersons.names, objPersons.surnames),
-    coatColor: getRandomChoise(objPersons.coatColors),
-    eyesColor: getRandomChoise(objPersons.eyesColors)
+    name: nameSurnameReverse(objPersons.names, objPersons.surnames),
+    coatColor: getRandomItem(objPersons.coatColors),
+    eyesColor: getRandomItem(objPersons.eyesColors)
   };
   return personData;
 };
 
-var pushPlayerData = function (players) {
-  // clon fragment
+var pushWizardData = function (players) {
   var clone = fragment.cloneNode(true);
-  // writnig wizards info
   var p = clone.querySelector('.setup-similar-label');
   p.textContent = players.name;
   var coatColorElement = clone.querySelector('.wizard-coat');
   coatColorElement.style.fill = players.coatColor;
   var eyesColorElement = clone.querySelector('.wizard-eyes');
   eyesColorElement.style.fill = players.eyesColor;
-  // push it to user
   var insertPlace = document.querySelector('.setup-similar-list');
   insertPlace.appendChild(clone);
 
@@ -57,20 +53,14 @@ var pushPlayerData = function (players) {
 
 // if browser knows content
 if ('content' in document.createElement('template')) {
-  // find first hidden area
   var hiddenObj = document.querySelector('.hidden');
-  // remove style hidden
   hiddenObj.classList.remove('hidden');
-  // read find template by id
   var fragment = document.querySelector('#similar-wizard-template').content;
-  // make person object and write in new massive randPersons
   var randPersons = [];
   for (var i = 0; i < NUMBER_OF_PIRSONS; i++) {
-    randPersons[i] = getRandPerson(PersonsData);
-    // construct template clone with randPersons data and push it to user
-    pushPlayerData(randPersons[i]);
+    randPersons[i] = getRandomWizard(PersonsData);
+    pushWizardData(randPersons[i]);
   }
-  // show players subbox
   var hiddenDiv = document.querySelector('.setup-similar');
   hiddenDiv.classList.remove('hidden');
 }
